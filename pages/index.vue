9f95eaece29f83905cd4348c93738827be543752
Adam Quinlan <aquinlan@tilsontech.com>
2020-05-20 12:15:45 -0400

    favicon

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,9 @@
       <h4>
         A landing page for designed for Tilson&#39;s broadband consulting clients.
       </h4>
+      <div class="iframecontainer">
       <iframe width="100%" height="650px" frameborder="0" src="https://tilson.speedtestcustom.com"></iframe>
+      </div>
     </div>
   </div>
 </template>
@@ -21,6 +23,10 @@ export default {
 .leading{
   text-align: center;
   margin-top: 50px;
+}
+.iframecontainer{
+  margin: 40px;
+  background-color: rgb(26, 26, 26);
 }
 
 </style>

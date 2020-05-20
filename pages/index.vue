f560e89739ef3da7cec625e882d2b6141220fe47
Adam Quinlan <aquinlan@tilsontech.com>
2020-05-20 11:38:55 -0400

    google fonts

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
-    <div>
-      <h1 class="title">
+    <div class="leading">
+      <h1>
         Tilson Speed Test
       </h1>
-      <h2 class="subtitle">
+      <h2>
         A landing page for Tilson&#39;s Broadband Consulting clients
       </h2>
       <iframe width="100%" height="650px" frameborder="0" src="https://tilson.speedtestcustom.com"></iframe>
@@ -17,6 +17,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.leading{
+  text-align: center;
+  margin-top: 50px;
+}
 
 </style>
+
+

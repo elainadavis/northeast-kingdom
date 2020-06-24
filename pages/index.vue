501abb61cfba4a902058f9b27cc8159de391f7c9
Adam Quinlan <aquinlan@tilsontech.com>
2020-06-23 22:41:41 -0400

    Main page changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,125 +1,56 @@
 <template>
   <div class="container">
     <div class="leading">
+      <div class="imhold">
+        <img src="~assets/images/client.jpg" />
+        +
+        <img src="~assets/images/tilson.png" />
+      </div>
       <h1>TWISP Broadband Initiative</h1>
-      <h4>Connecting Methow Valley to the 21st Century</h4>
-
-      <!-- START OF SPEED WIDGET -->
-      <div class="form-container">
-        <div id="sc-container">
-          <div id="sc-branding" class="sc-bb">
-            <a target="_blank" href="https://www.speedcheck.org/">
-              <img
-                src="https://cdn.speedcheck.org/branding/speedcheck-logo-18.png"
-                alt="Speedcheck"
-              />
-            </a>
-          </div>
-        </div>
-      </div>
-      <script
-        src="https://cdn.speedcheck.org/basic/scbjs.min.js"
-        async
-      ></script>
-      <div id="survey-panel" class="panel panel-white panel-no-border hide">
-        <div
-          id="survey123-webform"
-          class="panel panel-no-padding panel-no-border"
-        ></div>
-      </div>
-      <!-- <div>
-        <pre v-if="results">{{ results }}</pre>
-      </div> -->
-      <!-- <div class="iframecontainer"> -->
-      <!-- <iframe width="100%" height="650px" frameborder="0" src="https://tilson.speedtestcustom.com"></iframe> -->
-      <!-- </div> -->
+      <h3>Connecting Methow Valley to the 21st Century</h3>
+      <p>
+        Podunk unthaw owt Moody's robin showah noseeum gummah. Bangah junkah
+        wreckah Bangoah some wicked, down east out in th' willie-wacks dooryahd,
+        dingy hawsun around N'Hampshah gummah bookin' it aht mistah man. Nummah
+        than a faht dinnahbucket hahd tellin' not knowin' whawf out in th'
+        willie-wacks tunk what a cahd, wicked cunnin' bluebries Outta Staydahs
+        up t' junkah buggin' yessah-bub leaf peepahs fellers, yow uns Powrtland
+        Museum of Aht flatlandas heatah, stove up wickid decent owt junkah,
+        scrod lobstah paut Jeesum Crow junkah moose Moxie Moosetown, aht whawf
+        robin showah.
+      </p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  mounted: function() {
-    let vm = this; //Defining the vue instance
-
-    // Code to catch API call going out to grab the data collected by speedcheck.org
-    var open = window.XMLHttpRequest.prototype.open,
-      send = window.XMLHttpRequest.prototype.send;
-
-    function openReplacement(method, url, async, user, password) {
-      this._url = url;
-      return open.apply(this, arguments);
-    }
-
-    function sendReplacement(data) {
-      if (this._url == "https://api.speedspot.org/basic" && arguments[0]) {
-        console.log(arguments[0]);
-        vm.results = JSON.parse(arguments[0]);
-        vm.initializeForm(vm.results);
-      }
-      return send.apply(this, arguments);
-    }
-
-    window.XMLHttpRequest.prototype.open = openReplacement;
-    window.XMLHttpRequest.prototype.send = sendReplacement;
-
-  },
   data: function() {
-    return {
-      uploadSpeed: null,
-      downloadSpeed: null,
-      results: {}
-    };
+    return {};
   },
-  methods: {
-    initializeForm: function(speedData) {
-      const survey123WebForm = new Survey123WebForm({
-        container: "survey123-webform",
-        clientId: "Jy4JtM71ralXVggd",
-        portalUrl: "https://www.arcgis.com",
-        itemId: "f7d1b61670ec49788a8a9f246f5b2e9b",
-        onFormLoaded: data => {
-          //
-          // ANSWER INTERNET SPEED QUESTION WITH CONNECTION SPEED INFO //
-          //
-          survey123WebForm.setQuestionValue({
-            internet_speed: speedData.downloadValue
-          });
-        }
-      });
-      // SHOW SURVEY123 FORM //
-      let surveyPanel = document.getElementById("survey-panel");
-      surveyPanel.classList.remove("hide");
-    }
-  }
+  methods: {}
 };
 </script>
 
 <style lang="scss">
 .leading {
   text-align: center;
-  margin-top: 50px;
-}
-.iframecontainer {
-  margin: 40px;
-  background-color: rgb(26, 26, 26);
-}
-.form-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  #sc-container {
-    .sc-bb {
-      display: none;
-    }
-    .sc-button {
-      cursor: pointer;
-    }
-    #sc-basic-link {
-      display: none;
-    }
-    #sc-footer {
-      display: none;
+  max-width: 900px;
+  padding: 50px 20px 20px 20px;
+  margin: 0 auto;
+  p {
+    text-align: left;
+  }
+  .imhold {
+    font-size: 30px;
+    font-weight: 900;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    img {
+      width: 80px;
+      margin: 20px;
     }
   }
 }

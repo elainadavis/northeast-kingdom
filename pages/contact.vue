280223b48b0dc87ae7f3a6cf4c6f3a4687dffdbf
Adam Quinlan <quinlanadam@gmail.com>
2020-07-08 11:55:15 -0400

    page placeholders and basic structure

---
+++ pages/contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="gen-container">
+    <h1>Contact Us</h1>
+    <div class="contact-intro">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam mollis tortor et dolor sodales, at euismod ipsum pellentesque. Suspendisse mattis elit eget mauris aliquet elementum. Nunc finibus ante non tortor cursus, volutpat posuere est sollicitudin. Aliquam vel porttitor purus. Proin congue, augue nec iaculis commodo, massa mauris feugiat magna, et condimentum nunc dui vitae nibh. Cras auctor rhoncus odio eu ullamcorper. Donec consequat, tortor viverra cursus vehicula, lorem leo sodales ante, bibendum vulputate nisi mi ac est. Aenean in sapien ac ligula elementum auctor id vitae lectus.</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+};
+</script>
+<style lang="scss" scoped>
+.gen-container {
+  padding: 80px 15px;
+  max-width: 900px;
+  margin: 0 auto;
+  h1{
+    text-align: center;
+  }
+}
+</style>

20573438a197b1502a58659f643269bdd2522053
Adam Quinlan <aquinlan@tilsontech.com>
2020-07-09 19:54:37 -0400

    meta title and description pull from json

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,19 @@
+import generalData from './static/hubdata/general.json'
+
 export default {
   mode: "spa",
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || "",
+    title: generalData.projectName,
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || ""
+        content: generalData.projectByLine
       }
     ],
     link: [
